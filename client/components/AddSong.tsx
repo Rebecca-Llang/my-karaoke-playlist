e91a34e7787c6190e62f708a8664f3a00f8a0ad4
Rebecca Lang <rebeccalang50@gmail.com>
2024-08-28 12:00:19 +1200

    AddSong form rendering, and working on screen

--- client/components/AddSong.tsx
+++ client/components/AddSong.tsx
@@ -1,4 +1,104 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { ChangeEvent, FormEvent, useState } from 'react'
-import Song from '../../models/songs'
+import { Song } from '../../models/songs'
 import { addSong } from '../apis/songsAPI'
+
+function AddSong() {
+  // declare state for setting new song, set initial data object
+  // newSong can also be the destructed data {title, artist, genre, decade}
+  const [newSong, setNewSong] = useState({
+    title: '',
+    artist: '',
+    genre: '',
+    decade: 2000,
+  })
+
+  const queryClient = useQueryClient()
+
+  const addMutation = useMutation({
+    mutationFn: async (newSong: Song) => addSong(newSong),
+
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['songs'] })
+    },
+  })
+
+  const onChangeHandle = (event: ChangeEvent<HTMLInputElement>) => {
+    //  const { name, value } = evt.currentTarget
+    const key = event.target.id
+    const value = event.target.value
+
+    // returns newSong obj, key as id and value as input
+    const newSongObj = { ...newSong, [key]: value }
+
+    setNewSong(newSongObj)
+  }
+
+  // setFormValues makes shallow copy & add's new values to the array
+  //   setFormValues((previous) => ({
+  //     ...previous,
+  //     [name]: value,
+  //   }))
+  // }
+
+  const handleSubmit = async (evt: FormEvent<HTMLFormElement>) => {
+    evt.preventDefault()
+    await addMutation.mutate(newSong)
+  }
+
+  return (
+    <>
+      <form className="form" onSubmit={handleSubmit} aria-label="Add song">
+        <div>
+          <label htmlFor="title">Song Title</label>
+          <input
+            className="form__input"
+            type="text"
+            name="title"
+            id="title"
+            value={newSong.title}
+            onChange={onChangeHandle}
+          />
+        </div>
+        <div>
+          <label htmlFor="artist">Artist Name</label>
+          <input
+            className="form__input"
+            type="text"
+            name="artist"
+            id="artist"
+            value={newSong.artist}
+            onChange={onChangeHandle}
+          />
+        </div>
+        <div>
+          <label htmlFor="genre">Genre</label>
+          <input
+            className="form__input"
+            type="text"
+            name="genre"
+            id="genre"
+            value={newSong.genre}
+            onChange={onChangeHandle}
+          />
+        </div>
+        <div>
+          <label htmlFor="artist">Decade</label>
+          <input
+            className="form__input"
+            type="text"
+            name="decade"
+            id="decade"
+            value={newSong.decade}
+            onChange={onChangeHandle}
+          />
+        </div>
+
+        <button type="submit" className="button-primary">
+          Add Song
+        </button>
+      </form>
+    </>
+  )
+}
+export default AddSong
--- models/songs.ts
+++ models/songs.ts
@@ -1,12 +1,25 @@
 export interface Song {
-  id: number
+  id?: number
   title: string
   artist: string
   genre: string
   decade: number
 }
 
-export interface Songs {
-  id: number
-  songId: number
-}
+// export interface Songs {
+//   id: number
+//   songId: number
+// }
+
+// export interface User {
+//   id: number
+//   name: string
+//   email: string
+//   friendsId: number
+// }
+
+// export interface MyPlaylist {
+//   id: number
+//   userId: number
+//   mySongsIds: number
+// }
