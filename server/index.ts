c35d2b74f82168faa60bcc1cc8ba4d49d9b4af57
Rebecca Lang <rebeccalang50@gmail.com>
2024-09-01 11:14:54 +1200

    start of dokku set up, commit package-lock.json

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,7 @@
 import server from './server.ts'
 
-const PORT = process.env.PORT || 3000
+const port = process.env.PORT || 3000
 
-server.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log('Listening on port', PORT)
+server.listen(port, () => {
+  console.log(`Listening on port', ${port}`)
 })
