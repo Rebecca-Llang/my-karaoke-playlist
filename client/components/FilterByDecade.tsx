fe21fb765940a8e833ec847119545808debb60c6
Rebecca Lang <rebeccalang50@gmail.com>
2024-10-09 11:48:28 +1300

    filter my playlist by decade

---
+++ client/components/FilterByDecade.tsx
@@ -0,0 +1,35 @@
+import { Song } from '../../models/songs'
+
+interface Props {
+  data: Song[]
+  onDecadeChange: (decade: number) => void
+}
+
+export default function FilterByDecade({ onDecadeChange, data }: Props) {
+  const decades = new Set(
+    data.map((song) => song.decade).sort((min, max) => min - max),
+  )
+
+  const decadesArr = [...decades]
+
+  console.log('decades', decades)
+  console.log('decades arr', decadesArr)
+  return (
+    <>
+      <label htmlFor="decadeFilter">
+        <select
+          name="filterByDecade"
+          id="decadeFilter"
+          onChange={(e) => onDecadeChange(Number(e.target.value))}
+        >
+          <option value="">All Decades</option>
+          {decadesArr.map((decade) => (
+            <option key={decade} value={decade}>
+              {decade}s
+            </option>
+          ))}
+        </select>
+      </label>
+    </>
+  )
+}
--- client/components/Songs.tsx
+++ client/components/Songs.tsx
@@ -1,8 +1,11 @@
 import { useQuery } from '@tanstack/react-query'
 import { getAllSongs } from '../apis/songsAPI'
 import Song from './Song'
+import FilterByDecade from './FilterByDecade'
+import { useState } from 'react'
 
 function Songs() {
+  const [selectedDecade, setSelectedDecade] = useState<number | null>(null)
   const { data, isPending, isError, error } = useQuery({
     queryKey: ['songs'],
     queryFn: () => getAllSongs(),
@@ -17,12 +20,26 @@ function Songs() {
     return <p>Oh no! Error...</p>
   }
 
+  const filteredSongs = selectedDecade
+    ? data.filter((songs) => songs.decade === selectedDecade)
+    : data
+
+  console.log('data', data)
+
   return (
     <>
       <div className="songsPlaylist">
         <h3 id="songsPlaylist">My Songs:</h3>
+        <div className="filterDecade">
+          <h4>Filter By Decade:</h4>
+          <FilterByDecade
+            data={data}
+            onDecadeChange={(decade) => setSelectedDecade(decade)}
+          />
+        </div>
+
         <ul className="songList">
-          {data.map((song) => (
+          {filteredSongs.map((song) => (
             <Song
               key={song.id}
               id={song.id}
